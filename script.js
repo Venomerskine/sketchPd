4a42ce54537bbf0befc1947507721a33d7d7c3bd
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-09-04 11:01:00 +0300

    put all the functions in btn add eventlistener calculated the divs size by user pxl input removed the divs text content

--- script.js
+++ script.js
@@ -4,21 +4,28 @@ const pxlBtn = document.getElementById("pxlBtn")
 pxlBtn.addEventListener("click", () => {
     let num = prompt("Enter number of pixels. Not more than 100:")
     num = Number(num)
+
+    
      if (!isNaN(num) && num <= 100 && num > 0) {
       alert("You entered: " + num);
     } else {
       alert("Invalid input. Please enter a number between 1 and 100.");
     }
-})
+    const divSize = `calc(100% / ${num})`
 
-for (let i = 1; i <= 16 * 16; i++){
+    for (let i = 1; i <= num * num; i++){
     const div = document.createElement("div")
-    div.textContent = i
-     div.addEventListener('mouseenter', () => {
+        div.style.width = divSize
+        div.style.height = divSize
+        div.addEventListener('mouseenter', () => {
         div.style.backgroundColor = "#121b1b"
         div.style.color = "#0bcca2"
+        
     })
     cont.appendChild(div)
    
 }
+})
+
+
 
